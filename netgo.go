d91452d1f4efbad424dc69ccaf2c308d08e8c8fa
grt1st <grt1st@github.com>
2018-06-07 21:01:58 +0800

    优化服务端，增加html的get请求

--- netgo.go
+++ netgo.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"io"
+	"bufio"
 )
 
 const versionNumber = "1.0.0#20180606"
@@ -16,6 +17,7 @@ func main() {
 	listen := flag.Bool("l", false, "Listening on the server")
 	addr := flag.String("a", "", "Address to use")
 	port := flag.Int("p", 0, "Port to use ")
+	html := flag.Bool("html", false, "Send html request of GET")
 	help := flag.Bool("h", false, "Show usage")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage:\n  %s [Options]\n\nOptions\n", os.Args[0])
@@ -33,12 +35,10 @@ func main() {
 		return
 	}
 
-
-
 	if *listen {
 		listenS(*addr, *port)
 	}else {
-		connectS(*addr, *port)
+		connectS(*addr, *port, *html)
 	}
 
 }
@@ -50,29 +50,83 @@ func listenS(addr string, port int) {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", addr, port))
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
+
+	allClients := make(map[net.Conn]int)
+	newConnections := make(chan net.Conn)
+	deadConnections := make(chan net.Conn)
+	inMessages := make(chan string)
+	outMessages := make(chan string)
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			newConnections <- conn
+		}
+	}()
+
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			message, err := reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+			outMessages <- message
+		}
+	}()
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
+		select {
+		case conn := <-newConnections:
+			allClients[conn] = 1
+			go func(conn net.Conn) {
+				reader := bufio.NewReader(conn)
+				for {
+					message, err := reader.ReadString('\n')
+					if err != nil {
+						break
+					}
+					inMessages <- message
+				}
+				deadConnections <- conn
+			}(conn)
+		case message := <-inMessages:
+			fmt.Print(message)
+		case message := <-outMessages:
+			for c := range allClients{
+				go func(c net.Conn) {
+					_, err := c.Write([]byte(message))
+					if err != nil {
+						deadConnections <- c
+					}
+				}(c)
+			}
+		case conn := <-deadConnections:
+			delete(allClients, conn)
+			conn.Close()
 		}
-		go handleConn(conn)
 	}
+
+
 }
 
-func connectS(addr string, port int) {
+func connectS(addr string, port int, html bool) {
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", addr, port))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	go tran(os.Stdout, conn)
-	tran(conn, os.Stdin)
-}
-
-func handleConn(conn net.Conn) {
+	if html {
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+		io.Copy(os.Stdout, conn)
+		return
+	}
 	go tran(os.Stdout, conn)
 	tran(conn, os.Stdin)
 }
